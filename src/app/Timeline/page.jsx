950854f285db56118fc820377c294b51f35e7419
Nick-roy <nroy08232@gmail.com>
2026-04-17 09:12:08 +0600

    final push

--- src/app/Timeline/page.jsx
+++ src/app/Timeline/page.jsx
@@ -1,13 +1,17 @@
 'use client'
 
-import { useContext } from "react"
-
+import { useContext, useState } from "react"
 import TimeCard from "../ui/TimeCard"
 import { FriendApi } from "@/context/ContextApi"
 
-
 const Timeline = () => {
   const { calls } = useContext(FriendApi)
+  const [filter, setFilter] = useState("all")
+
+  const filteredCalls =
+    filter === "all"
+      ? calls
+      : calls.filter(call => call.type === filter)
 
   if (!calls || calls.length === 0) {
     return (
@@ -22,9 +26,22 @@ const Timeline = () => {
     <div className="w-10/12 mx-auto p-5">
       <h1 className="text-3xl font-bold mb-4">Timeline</h1>
 
+<div className="dropdown dropdown-center">
+  <div tabIndex={0} role="button" className="btn m-1">
+     Search by   <span className="text-green-900">{filter}</span>
+  </div>
+
+  <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li onClick={() => setFilter('all')}><a>All</a></li>
+    <li onClick={() => setFilter('call')}><a>Call</a></li>
+    <li onClick={() => setFilter('video')}><a>Video</a></li>
+    <li onClick={() => setFilter('chat')}><a>Chat</a></li>
+  </ul>
+</div>
+
       <div className="shadow-sm rounded-2xl p-4 space-y-3">
         {
-          calls.map((call, i) => (
+          filteredCalls.map((call, i) => (
             <TimeCard key={call.id || i} call={call} />
           ))
         }
